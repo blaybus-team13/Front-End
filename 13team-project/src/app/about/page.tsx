a3e0503b2ce35978d6c2963e108ef3003bb0c768
최지한 <78947716+HANN-Creator@users.noreply.github.com>
2025-02-18 22:20:44 +0900

    Delete 13team-project/src/app directory

--- 13team-project/src/app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function about() {
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        <h1 className="text-3xl font-bold">대시보드 페이지</h1>
-        <p>이 페이지가 정상적으로 렌더링됩니다.</p>
-      </div>
-    );
-  }
-  
